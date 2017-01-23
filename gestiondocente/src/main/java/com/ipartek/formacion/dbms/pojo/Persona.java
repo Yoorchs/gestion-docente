5b865b44cb4dc3fe9ceb2e1e0e4bbaaf7fcd5b4b
Yoorchs <elmonker@gmail.com>
2017-01-23 13:28:38 +0100

    Corregido Validar DNI

--- gestiondocente/src/main/java/com/ipartek/formacion/dbms/pojo/Persona.java
+++ gestiondocente/src/main/java/com/ipartek/formacion/dbms/pojo/Persona.java
@@ -67,7 +67,7 @@ public class Persona {
 		final String regex = "\\d{8}[A-Za-z]";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(dni);
-		if (!matcher.find() && !Util.validarDni(dni)) {
+		if (!matcher.find() && Util.validarDni(dni)) {
 			throw new PersonaException(PersonaException.COD_DNI_ERROR, PersonaException.MSG_DNI_ERROR);
 		}
 		this.dni = dni;
